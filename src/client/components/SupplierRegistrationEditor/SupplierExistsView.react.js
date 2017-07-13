60ffbb23b8c13895765fa1f40b74fcb6940cea8b
Lekealem Asong <lekealemasong@yahoo.de>
2017-07-13 13:09:56 +0200

    Refactor text

--- src/client/components/SupplierRegistrationEditor/SupplierExistsView.react.js
+++ src/client/components/SupplierRegistrationEditor/SupplierExistsView.react.js
@@ -13,7 +13,7 @@ class SupplierExistsView extends Component {
   render() {
     return (
       <div class="jumbotron">
-        <h3>{this.props.i18n.getMessage('SupplierRegistrationEditor.Messages.supplierExistsHeader')}</h3>
+        <h4>{this.props.i18n.getMessage('SupplierRegistrationEditor.Messages.supplierExistsHeader')}</h4>
         <p>{this.props.i18n.getMessage('SupplierRegistrationEditor.Messages.supplierExistsText')}</p>
         <button className="btn btn-default" onClick={ this.handleClick }>
           {this.props.i18n.getMessage('SupplierRegistrationEditor.ButtonLabel.back')}
