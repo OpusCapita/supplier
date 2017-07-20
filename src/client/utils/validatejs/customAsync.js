55599eb99f9f5c46de539b0a37f5a7664b0aad6a
Lekealem Asong <lekealemasong@yahoo.de>
2017-07-20 12:24:47 +0200

    Refactoring

--- src/client/utils/validatejs/customAsync.js
+++ src/client/utils/validatejs/customAsync.js
@@ -6,27 +6,24 @@ const url = '/supplier/api/suppliers/exists';
 module.exports.vatNumberExists = function(validate) {
   return validate.validators.vatNumberExists = function(value, options, key, attributes) {
     let queryParams = { vatIdentificationNo: value };
-    if (attributes['supplierId']) queryParams.supplierId = attributes['supplierId'];
 
-    return recordExists(value, validate, queryParams, options.message);
+    return recordExists(value, validate, queryParams, options.message, attributes['supplierId']);
   };
 };
 
 module.exports.dunsNumberExists = function(validate) {
   return validate.validators.dunsNumberExists = function(value, options, key, attributes) {
     let queryParams = { dunsNo: value };
-    if (attributes['supplierId']) queryParams.supplierId = attributes['supplierId'];
 
-    return recordExists(value, validate, queryParams, options.message);
+    return recordExists(value, validate, queryParams, options.message, attributes['supplierId']);
   };
 };
 
 module.exports.globalLocationNumberExists = function(validate) {
   return validate.validators.globalLocationNumberExists = function(value, options, key, attributes) {
     let queryParams = { globalLocationNo: value }
-    if (attributes['supplierId']) queryParams.supplierId = attributes['supplierId'];
 
-    return recordExists(value, validate, queryParams, options.message);
+    return recordExists(value, validate, queryParams, options.message, attributes['supplierId']);
   };
 };
 
@@ -36,9 +33,8 @@ module.exports.taxIdNumberExists = function(validate) {
       taxIdentificationNo: attributes['taxIdentificationNo'],
       countryOfRegistration: attributes['countryOfRegistration']
     };
-    if (attributes['supplierId']) queryParams.supplierId = attributes['supplierId'];
 
-    return recordExists(attributes['taxIdentificationNo'], validate, queryParams, options.message);
+    return recordExists(attributes['taxIdentificationNo'], validate, queryParams, options.message, attributes['supplierId']);
   };
 };
 
@@ -49,18 +45,19 @@ module.exports.registerationNumberExists = function(validate) {
       cityOfRegistration: attributes['cityOfRegistration'],
       countryOfRegistration: attributes['countryOfRegistration']
     };
-    if (attributes['supplierId']) queryParams.supplierId = attributes['supplierId'];
 
-    return recordExists(attributes['commercialRegisterNo'], validate, queryParams, options.message);
+    return recordExists(attributes['commercialRegisterNo'], validate, queryParams, options.message, attributes['supplierId']);
   };
 };
 
-let recordExists = function(value, validate, queryParams, errorMessage) {
+let recordExists = function(value, validate, queryParams, errorMessage, supplierId) {
   return new validate.Promise((resolve, reject) => {
     if (!value) {
       resolve();
       return;
     }
+
+    if (supplierId) queryParams.supplierId = supplierId;
 
     request.get(url).query(queryParams).set('Accept', 'application/json').then(response => {
       if (response.body) {
