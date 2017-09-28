9fafe2ad6fe7dcf5cc7e72b66da76bdc49d678e6
Konrad Szałwiński <konrad.szalwinski@gmail.com>
2017-09-28 13:24:35 +0200

    Responsive styling

--- src/client/components/SupplierSearch/SupplierSearch.react.js
+++ src/client/components/SupplierSearch/SupplierSearch.react.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import i18nMessages from './i18n';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
+import styles from './SupplierSearch.css';
 
 export default class SupplierSearch extends Component {
 
@@ -16,6 +17,30 @@ export default class SupplierSearch extends Component {
 
   componentWillMount(){
     this.context.i18n.register('SupplierSearch', i18nMessages);
+  }
+
+  renderSearchBox() {
+    return (<div className="form-group search-box">
+      <label className="col-xs-12 col-sm-6 col-md-4 control-label">Company Name</label>
+      <input className="form-control"/>
+      <label className="col-xs-12 col-sm-6 col-md-4 control-label">Registration Number</label>
+      <input className="form-control"/>
+      <label className="col-xs-12 col-sm-6 col-md-4 control-label">City of Registration</label>
+      <input className="form-control"/>
+      <label className="col-xs-12 col-sm-6 col-md-4 control-label">Country of Registration</label>
+      <input className="form-control"/>
+      <label className="col-xs-12 col-sm-6 col-md-4 control-label">Tax Identification Number</label>
+      <input className="form-control"/>
+      <label className="col-xs-12 col-sm-6 col-md-4 control-label">VAT Identification Number</label>
+      <input className="form-control"/>
+      <label className="col-xs-12 col-sm-6 col-md-4 control-label">Global Location Number</label>
+      <input className="form-control"/>
+      <label className="col-xs-12 col-sm-6 col-md-4control-label">D-U-N-S Number</label><input className="form-control"/>
+      <div className="text-right form-submit">
+        <button className="btn btn-link">Cancel</button>
+        <button className="btn btn-primary">Search</button>
+      </div>
+    </div>)
   }
 
   renderTable() {
@@ -59,13 +84,17 @@ export default class SupplierSearch extends Component {
     return (<ReactTable
       data={data}
       columns={columns}
+      defaultPageSize={5}
+      className="table"
     />)
   }
 
   render() {
-    return (
+    return (<div>
+      { this.renderSearchBox() }
       <div className="table-responsive">
-          { this.renderTable() }
-      </div>)
+        { this.renderTable() }
+      </div>
+    </div>)
   }
 }
