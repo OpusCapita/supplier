5f8cb2d5d75826e6a761edc21d8e44b78c003952
Lekealem Asong <lekealemasong@yahoo.de>
2017-11-13 10:11:35 +0100

    Add endpoint to create user from contact

--- src/server/routes/supplier_contacts.js
+++ src/server/routes/supplier_contacts.js
@@ -1,4 +1,6 @@
 const SupplierContact = require('../queries/supplier_contacts');
+const userService = require('../services/user');
+const authService = require('../services/auth');
 
 module.exports = function(app, db, config) {
   SupplierContact.init(db, config).then(() =>
@@ -37,32 +39,53 @@ let createSupplierContact = function(req, res)
     req.opuscapita.logger.error('Error when creating SupplierContact: %s', error.message);
     return res.status('400').json({ message : error.message });
   });
-}
+};
 
 let createUserFromContact = function(req, res)
 {
-}
+  const contact = req.body;
+  return SupplierContact.contactExists(req.params.supplierId, contact.id).then(exists => {
+    if (!exists) return handleContactNotExistError(res, contact.id, req.opuscapita.logger);
+
+    return userService.getProfile(req.opuscapita.serviceClient, contact.email).then(userProfile => {
+      const attributes = {
+        firstName: userProfile.firstName,
+        lastName: userProfile.lastName,
+        email: userProfile.email,
+        title: userProfile.salutation,
+        phone: userProfile.phoneNo,
+        isLinkedToUser: true
+      };
+      return updateAndRenderContact(contact.supplierId, contact.id, attributes, res);
+    }).catch(error => {
+      if (error.response && error.response.statusCode == '404') {
+        const authUser = { firstName: contact.firstName, lastName: contact.lastName, email: contact.email };
+        authService.createUser(req.opuscapita.serviceClient, authUser).then(() => {
+          return updateAndRenderContact(contact.supplierId, contact.id, { isLinkedToUser: true }, res);
+        });
+      } else {
+        req.opuscapita.logger.error('Error when creating user from SupplierContact: %s', error.message);
+        return res.status('400').json({ message : error.message });
+      }
+    });
+  });
+};
 
 let updateSupplierContact = function(req, res)
 {
-  let contactId = decodeURIComponent(req.params.contactId);
-  let supplierId = req.params.supplierId;
-  SupplierContact.contactExists(supplierId, contactId).then(exists =>
+  const contactId = decodeURIComponent(req.params.contactId);
+  const supplierId = req.params.supplierId;
+  return SupplierContact.contactExists(supplierId, contactId).then(exists =>
   {
-    if(exists)
-    {
-      return SupplierContact.update(supplierId, contactId, req.body).then(contact => res.status('200').json(contact));
-    } else {
-      const message = 'A supplier contact with ID ' + contactId + ' does not exist.'
-      req.opuscapita.logger.error('Error when updating SupplierContact: %s', message);
-      return res.status('404').json({ message : message });
-    }
+    if(!exists) return handleContactNotExistError(res, contactId, req.opuscapita.logger);
+
+    return updateAndRenderContact(supplierId, contactId, req.body, res);
   })
   .catch(error => {
     req.opuscapita.logger.error('Error when updating SupplierContact: %s', error.message);
     return res.status('400').json({ message : error.message });
   });
-}
+};
 
 let deleteSupplierContact = function(req, res)
 {
@@ -72,4 +95,14 @@ let deleteSupplierContact = function(req, res)
     req.opuscapita.logger.error('Error when deleting SupplierContact: %s', error.message);
     return res.status('400').json({ message : error.message });
   });
-}
+};
+
+let handleContactNotExistError = function(response, contactId, logger) {
+  const message = 'A supplier contact with ID ' + contactId + ' does not exist.'
+  logger.error('Error when updating SupplierContact: %s', message);
+  return response.status('404').json({ message : message });
+};
+
+let updateAndRenderContact = function(supplierId, contactId, attributes, response) {
+  return SupplierContact.update(supplierId, contactId, attributes).then(contact => response.status('200').json(contact));
+};
