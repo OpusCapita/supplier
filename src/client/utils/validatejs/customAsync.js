fabee9355e35f529d607aecf81053c156a62a03e
Lekealem Asong <lekealemasong@yahoo.de>
2017-12-12 11:02:56 +0100

    Add uniqueIdentifierWithBankAccount constraint to supplier

--- src/client/utils/validatejs/customAsync.js
+++ src/client/utils/validatejs/customAsync.js
@@ -1,12 +1,13 @@
 const request = require('superagent-bluebird-promise');
-const { Supplier } = require('../../api');
+const { Supplier, BankAccount } = require('../../api');
 const supplierApi = new Supplier();
+const bankAccountApi = new BankAccount();
 
 module.exports.supplierNameExists = function(validate) {
   return validate.validators.supplierNameExists = function(value, options, key, attributes) {
     let queryParams = { supplierName: value };
 
-    return recordExists(value, validate, queryParams, options.message, attributes['supplierId']);
+    return recordExists(value, validate, queryParams, options.message, attributes.supplierId);
   };
 };
 
@@ -14,7 +15,7 @@ module.exports.vatNumberExists = function(validate) {
   return validate.validators.vatNumberExists = function(value, options, key, attributes) {
     let queryParams = { vatIdentificationNo: value };
 
-    return recordExists(value, validate, queryParams, options.message, attributes['supplierId']);
+    return recordExists(value, validate, queryParams, options.message, attributes.supplierId);
   };
 };
 
@@ -22,7 +23,7 @@ module.exports.dunsNumberExists = function(validate) {
   return validate.validators.dunsNumberExists = function(value, options, key, attributes) {
     let queryParams = { dunsNo: value };
 
-    return recordExists(value, validate, queryParams, options.message, attributes['supplierId']);
+    return recordExists(value, validate, queryParams, options.message, attributes.supplierId);
   };
 };
 
@@ -30,7 +31,7 @@ module.exports.globalLocationNumberExists = function(validate) {
   return validate.validators.globalLocationNumberExists = function(value, options, key, attributes) {
     let queryParams = { globalLocationNo: value }
 
-    return recordExists(value, validate, queryParams, options.message, attributes['supplierId']);
+    return recordExists(value, validate, queryParams, options.message, attributes.supplierId);
   };
 };
 
@@ -38,39 +39,56 @@ module.exports.ibanExists = function(validate) {
   return validate.validators.ibanExists = function(value, options, key, attributes) {
     let queryParams = { iban: value }
 
-    return recordExists(value, validate, queryParams, options.message, attributes['supplierId']);
+    return recordExists(value, validate, queryParams, options.message, attributes.supplierId);
   };
 };
 
 module.exports.taxIdNumberExists = function(validate) {
   return validate.validators.taxIdNumberExists = function(value, options, key, attributes) {
     let queryParams = {
-      taxIdentificationNo: attributes['taxIdentificationNo'],
-      cityOfRegistration: attributes['cityOfRegistration']
+      taxIdentificationNo: attributes.taxIdentificationNo,
+      cityOfRegistration: attributes.cityOfRegistration
     };
 
-    return recordExists(attributes['taxIdentificationNo'], validate, queryParams, options.message, attributes['supplierId']);
+    return recordExists(attributes.taxIdentificationNo, validate, queryParams, options.message, attributes.supplierId);
   };
 };
 
 module.exports.registerationNumberExists = function(validate) {
   return validate.validators.registerationNumberExists = function(value, options, key, attributes) {
     let queryParams = {
-      commercialRegisterNo: attributes['commercialRegisterNo'],
-      cityOfRegistration: attributes['cityOfRegistration'],
-      countryOfRegistration: attributes['countryOfRegistration']
+      commercialRegisterNo: attributes.commercialRegisterNo,
+      cityOfRegistration: attributes.cityOfRegistration,
+      countryOfRegistration: attributes.countryOfRegistration
     };
 
-    return recordExists(attributes['commercialRegisterNo'], validate, queryParams, options.message, attributes['supplierId']);
+    return recordExists(attributes.commercialRegisterNo, validate, queryParams, options.message, attributes.supplierId);
+  };
+};
+
+module.exports.uniqueIdentifierWithBankAccount = function(validate) {
+  return validate.validators.uniqueIdentifierWithBankAccount = function(value, options, key, attributes) {
+    return new validate.Promise((resolve, reject) => {
+      const uniqueIdentifier = require('../../../server/utils/validators/uniqueIdentifier.js');
+      const fields = [attributes.vatIdentificationNo, attributes.dunsNo, attributes.globalLocationNo];
+      if (uniqueIdentifier.isValid(fields)) { resolve(); return; }
+
+      if (!attributes.supplierId) { resolve(); return; }
+
+      bankAccountApi.getBankAccounts(attributes.supplierId).then(accounts => {
+        if (accounts.length > 0) {
+          resolve();
+        } else {
+          resolve(options.message);
+        }
+      }).catch(error => reject());
+    });
   };
 };
 
 let recordExists = function(value, validate, queryParams, errorMessage, supplierId) {
   return new validate.Promise((resolve, reject) => {
-    if (!value) {
-      resolve();
-      return;
-    }
+    if (!value) { resolve(); return; }
 
     if (supplierId) queryParams.supplierId = supplierId;
 
