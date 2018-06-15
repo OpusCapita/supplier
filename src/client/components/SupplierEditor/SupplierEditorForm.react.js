d1be64df7fadc16e3526c8f5d6dec1d4f88280e4
Lekealem Asong <lekealemasong@yahoo.de>
2018-06-15 12:11:50 +0200

    Fix bug in datepicker translations

--- src/client/components/SupplierEditor/SupplierEditorForm.react.js
+++ src/client/components/SupplierEditor/SupplierEditorForm.react.js
@@ -228,7 +228,7 @@ class SupplierEditorForm extends Component {
             component: (
               <DateInput
                 className="form-control"
-                locale={i18n.locale}
+                locale={['en', 'de'].includes(i18n.locale) ? i18n.locale : 'en'}
                 dateFormat={i18n.dateFormat}
                 value={foundedOn}
                 onChange={this.handleChange.bind(this, 'foundedOn')}
