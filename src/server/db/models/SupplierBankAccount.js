4b5be1ec0f3022d6b21f24ceed8a3d644c0879a9
Lekealem Asong <lekealemasong@yahoo.de>
2018-09-13 16:48:42 +0200

    Change isr number validation

--- src/server/db/models/SupplierBankAccount.js
+++ src/server/db/models/SupplierBankAccount.js
@@ -70,10 +70,15 @@ module.exports.init = function (db, config) {
         field: 'Plusgiro',
         type: Sequelize.STRING(30)
       },
-      /** A 9-digit ISR party number under Swiss IBAN */
+      /** A maximum 9-digit ISR party number under Swiss IBAN. Switzerland has special legal banking
+      requirements such as to print ISR Payment Slip from sales Invoices. The ISR Payment Slip is an
+      In-payment Slip with Reference Number (ISR or ESR in Swiss German) to facilitate customer to
+      pay Invoice in Swiss Franc or in Euro in simple manner.
+      References: https://www.six-group.com/interbank-clearing/dam/downloads/en/standardization/iso/swiss-recommendations/archives/swiss-usage-guide.pdf
+      and https://advendio.atlassian.net/wiki/spaces/SO/pages/130195958/6.1.9+Generate+Swiss+ISR+Code+ESR+Code */
       isrNumber: {
         field: 'ISRNumber',
-        type: Sequelize.STRING(9),
+        type: Sequelize.STRING(11),
         validate: {
           isValid(value) {
             if (!value) return null;
