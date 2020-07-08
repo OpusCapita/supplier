f9f2665fa2b79fc38fd6a65f06818ad6610125d8
nczeranka <norbert.czeranka@opuscapita.com>
2020-07-08 10:46:20 +0200

    auto import from translation excel

--- src/client/i18n/index.js
+++ src/client/i18n/index.js
@@ -2,11 +2,6 @@
 
 const de = require('./de.json');
 const en = require('./en.json');
-const es = require('./es.json');
-const fi = require('./fi.json');
-const fr = require('./fr.json');
-const it = require('./it.json');
-const pt = require('./pt.json');
-const sv = require('./sv.json');
+const pl = require('./pl.json');
 
-module.exports = {de, en, es, fi, fr, it, pt, sv};
+module.exports = {de, en, pl};
